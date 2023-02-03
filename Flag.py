8c0bf385d48604c153773ab37bb5467d346bc1b3
Ritvik003 <36434573+Ritvik003@users.noreply.github.com>
2023-02-02 21:47:54 -0700

    Add files via upload

---
+++ Flag.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+#enter in dummy url for now, this will be modified in chrome extension to accept any url
+
+#url = "https://www.azfamily.com/2023/01/27/arizona-dps-troopers-bust-over-160-pounds-drugs-3-traffic-stops/"
+
+url = "https://www.pornhub.com/view_video.php?viewkey=ph63b4cd6330ad1"
+
+#this is a very simple content restriction program
+
+def flag_text(host) :
+    buzz = ["enter explicit buzzwords here"]
+    page = urlopen(host)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+
+    text = soup.get_text()
+    print(text)
+    
+    for word in buzz:
+        if text.find(word) != -1:
+            print("hi")
+            return True
+    
+    return False
+        
+flag_text(url)
+
+
